9668aa45e3cb20cf638e0193ba58aedd3c456754
michuMF <michu.ferenc@gmail.com>
2024-06-17 18:08:04 +0200

    change task background to red

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,3 @@
-import PropTypes from "prop-types"
-
 export default function Task({
 	task: { id, title, state },
 	onArchiveTask,
@@ -29,8 +27,10 @@ export default function Task({
 					name='title'
 					id={`title-${id}`}
 					placeholder='Input title'
+					style={{ backgroundColor: "red" }}
 				/>
 			</label>
+
 			{state !== "TASK_ARCHIVED" && (
 				<button
 					className='pin-button'
@@ -43,19 +43,4 @@ export default function Task({
 			)}
 		</div>
 	)
-}
-Task.propTypes = {
-	/** Composition of the task */
-	task: PropTypes.shape({
-		/** Id of the task */
-		id: PropTypes.string.isRequired,
-		/** Title of the task */
-		title: PropTypes.string.isRequired,
-		/** Current state of the task */
-		state: PropTypes.string.isRequired,
-	}),
-	/** Event to change the task to archived */
-	onArchiveTask: PropTypes.func,
-	/** Event to change the task to pinned */
-	onPinTask: PropTypes.func,
 }
